91d3c3d6b74dd6b997383536b3d7004f828f1734
MRXPRO200330020179Y <mrxpro2003@gmail.com>
2022-12-16 11:11:50 +0500

    узгаришлар

--- dict/js/index.js
+++ dict/js/index.js
@@ -1,14 +1,27 @@
 $(document).ready(function(){
-      document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.sidenav');
-        var instances = M.Sidenav.init(elems, options);
-      });
-    
-      // Or with jQuery
-    
-      $(document).ready(function(){
-        $('.sidenav').sidenav();
-      });
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.sidenav');
+      var instances = M.Sidenav.init(elems, options);
+    });
+        
+    $(document).ready(function(){
+      $('.sidenav').sidenav();
+    });
+
+// -------------------------------===========-------------------------------
+
+    $(document).ready(function(){
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.modal');
+            var instances = M.Modal.init(elems, options);
+          });
+    });
+  
+    $(document).ready(function(){
+        $(document).ready(function(){
+            $('.modal').modal();
+        });
+    });
 });
 
 // -------------------------------===========-------------------------------
@@ -75,3 +88,37 @@ else
 });
 
 // -------------------------------===========-------------------------------
+
+
+// =======------------------------------=======------------------------=====
+
+var i=0;
+	$(".slider__menu__link").each(function(){
+  length
+	i++;
+	$(this).attr("href","#mrx-"+i);
+});
+
+var i=0;
+	$(".slider__item").each(function(){
+  length
+	i++;
+	$(this).attr("id","mrx-"+i);
+});
+
+
+// directions_in id end
+$('.slider__list .slider__item').hide();
+$('.slider__list .slider__item:first').show();
+$('.slider__menu li:first').addClass('active');
+
+// Change tab class and display content
+$('.slider__menu a').on('click', function(event){
+  event.preventDefault();
+  $('.slider__menu li').removeClass('active');
+  $(this).parent().addClass('active');
+  $('.slider__list .slider__item').hide();
+  $($(this).attr('href')).show();
+});
+
+// =======------------------------------=======------------------------=====
--- dict/js/slic.js
+++ dict/js/slic.js
@@ -10,6 +10,16 @@ $(document).ready(function(){
         fade: true,
     });
 
+    $('.history_uzbekistan__list').slick({
+      dots: false,
+      arrows:true,
+      autoplaySpeed:5000,
+      infinite: true,
+      speed: 1500,
+      autoplay:true,
+      fade: true,
+    });
+
     $('.search_results_in__carusel__cart').slick({
         dots: false,
         arrows:true,
@@ -51,5 +61,69 @@ $(document).ready(function(){
           },
         ]
     });
-        
+
+    $('.owl-carousel').owlCarousel({
+      loop:false,
+      margin:10,
+      data:false,
+      nav:true,
+      autoplaySpeed:2000,
+      autoplay:true,
+        responsive:{
+          0:{
+            items:1
+          },
+
+          550:{
+            items:2
+          },
+
+          750:{
+            items:3
+          },
+
+          1010:{
+            items:4
+          },
+    
+          1250:{
+            items:4
+          }
+      }
+    });
+
+    $('.electronic_library__list').slick({
+      rows: 2,
+      dots: false,
+      arrows: true,
+      infinite: true,
+      autoplaySpeed:5000,
+      speed: 1500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+    
+      responsive: [
+        {
+          breakpoint: 1010,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 2,
+          }
+        },
+        {
+          breakpoint: 750,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 550,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        },
+      ]
+    }); 
 })
